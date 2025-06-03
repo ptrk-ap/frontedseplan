1c0f08be6fec431c05a347096260d5af3fd49767
ptrk-ap <patrickms.ap1997@outlook.com>
2025-06-03 15:33:52 -0300

    Descreva aqui o que mudou, ex: Ajusta componente Header

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -1,7 +1,7 @@
 // services/dashboardService.js
 import axios from 'axios';
 
-export const fetchGrafico1 = () => axios.get('http://localhost:3000/get-data').then(res => res.data);
-export const fetchGrafico2 = () => axios.get('http://localhost:3000/get-ug').then(res => res.data);
-export const fetchGrafico3 = () => axios.get('http://localhost:3000/get-receita').then(res => res.data);
+export const fetchGrafico1 = () => axios.get('https://painelseplan.onrender.com/get-data').then(res => res.data);
+export const fetchGrafico2 = () => axios.get('https://painelseplan.onrender.com/get-ug').then(res => res.data);
+export const fetchGrafico3 = () => axios.get('https://painelseplan.onrender.com//get-receita').then(res => res.data);
 
