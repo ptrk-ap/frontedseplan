d0d9a1c19ebe6f5f1f1381f906644dc6e6c4be29
ptrk-ap <patrickms.ap1997@outlook.com>
2025-06-04 09:16:03 -0300

    mensagem explicativa do commit

--- src/services/dashboardService.js
+++ src/services/dashboardService.js
@@ -3,5 +3,5 @@ import axios from 'axios';
 
 export const fetchGrafico1 = () => axios.get('https://painelseplan.onrender.com/get-data').then(res => res.data);
 export const fetchGrafico2 = () => axios.get('https://painelseplan.onrender.com/get-ug').then(res => res.data);
-export const fetchGrafico3 = () => axios.get('https://painelseplan.onrender.com//get-receita').then(res => res.data);
+export const fetchGrafico3 = () => axios.get('https://painelseplan.onrender.com/get-receita').then(res => res.data);
 
